521ff93a58b5d456bd427d9a671b012188727f4b
Baranov Denis <baranovdenis1983@gmail.com>
2023-09-10 20:25:07 +0300

    Добавил схему MoneyTable и внес в DataBaseHelper код для создания таблицы

--- app/src/main/java/com/example/schoolmoney/database/DbSchema.java
+++ app/src/main/java/com/example/schoolmoney/database/DbSchema.java
@@ -4,9 +4,7 @@ public class DbSchema {
 
 
     public static final class ChildTable {
-
         public static final String NAME = "Children";
-
         public static final class Cols {
             public static final String UUID = "uuid";
             public static final String CHILD_NAME = "child_name";
@@ -14,9 +12,7 @@ public class DbSchema {
     }
 
     public static final class ParentTable {
-
         public static final String NAME = "Parents";
-
         public static final class Cols {
             public static final String CHILD_UUID = "child_uuid";
             public static final String PARENT_NAME = "parent_name";
@@ -25,12 +21,22 @@ public class DbSchema {
     }
 
     public static final class NoteTable {
-
         public static final String NAME = "Notes";
-
         public static final class Cols {
             public static final String CHILD_UUID = "child_uuid";
             public static final String NOTE = "note";
+        }
+    }
+
+    public static final class MoneyTable {
+        public static final String NAME = "Money";
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String NOTE = "note";
+            public static final String VALUE_INCOME = "value_income";
+            public static final String VALUE_EXPENSES = "value_expenses";
+            public static final String DATE = "date";
         }
     }
 
